9992acbb4eb9c2eff41fe119258f2ae8a67574e6
Pascal Wallenius <pascal.wallenius@volvocars.com>
2023-01-03 11:34:20 +0100

    No swap file

--- lua/pwalleni/core/options.lua
+++ lua/pwalleni/core/options.lua
@@ -30,3 +30,8 @@ opt.splitright = true
 opt.splitbelow = true
 
 opt.iskeyword:append("-")
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
