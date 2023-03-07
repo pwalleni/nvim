a987b08767c7b436355ec13f2b8bf6c7e75cf89d
Pascal Wallenius <pascal.wallenius@volvocars.com>
2023-03-07 18:11:14 +0100

    Gruvbox in da house

--- lua/pwalleni/plugins-setup.lua
+++ lua/pwalleni/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -20,115 +20,116 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-    return
+	return
 end
 
 return packer.startup(function(use)
-    use("wbthomason/packer.nvim")
+	use("wbthomason/packer.nvim")
 
-    use("nvim-lua/plenary.nvim")
+	use("nvim-lua/plenary.nvim")
 
---  use("Mofiqul/dracula.nvim")
-    use("bluz71/vim-nightfly-guicolors")
+	--  use("Mofiqul/dracula.nvim")
+	-- use("bluz71/vim-nightfly-guicolors")
+	use({ "ellisonleao/gruvbox.nvim" })
 
-    use("christoomey/vim-tmux-navigator")
+	use("christoomey/vim-tmux-navigator")
 
-    use("szw/vim-maximizer")
+	use("szw/vim-maximizer")
 
-    use("tpope/vim-surround")
+	use("tpope/vim-surround")
 
-    use("vim-scripts/ReplaceWithRegister")
+	use("vim-scripts/ReplaceWithRegister")
 
-    use("numToStr/Comment.nvim")
+	use("numToStr/Comment.nvim")
 
-    use("kyazdani42/nvim-web-devicons")
+	use("kyazdani42/nvim-web-devicons")
 
-    use("nvim-lualine/lualine.nvim")
+	use("nvim-lualine/lualine.nvim")
 
-    -- fuzzy finding
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+	-- fuzzy finding
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
-    -- autocompletion
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
+	-- autocompletion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
 
-    -- snippets
-    use("L3MON4D3/LuaSnip")
-    use("saadparwaiz1/cmp_luasnip")
-    use("rafamadriz/friendly-snippets")
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
 
-    -- LSP managing and installing servers
-    use({ "williamboman/mason.nvim" })
-    use("williamboman/mason-lspconfig.nvim")
+	-- LSP managing and installing servers
+	use({ "williamboman/mason.nvim" })
+	use("williamboman/mason-lspconfig.nvim")
 
-    -- LSP configuring servers
-    use("neovim/nvim-lspconfig")
-    use("hrsh7th/cmp-nvim-lsp")
-    -- use({ "glepnir/lspsaga.nvim", branch = "main" })
-    use("jose-elias-alvarez/typescript.nvim")
-    use("onsails/lspkind.nvim")
+	-- LSP configuring servers
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	-- use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use("jose-elias-alvarez/typescript.nvim")
+	use("onsails/lspkind.nvim")
 
-    -- formating and linting
-    use("jose-elias-alvarez/null-ls.nvim")
-    use("jayp0521/mason-null-ls.nvim")
+	-- formating and linting
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
 
-    -- formating and linting
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = function()
-            require("nvim-treesitter.install").update({ with_sync = true })
-        end,
-    })
+	-- formating and linting
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
 
-    -- auto closing
-    use("windwp/nvim-ts-autotag")
+	-- auto closing
+	use("windwp/nvim-ts-autotag")
 
-    -- gitsigns
-    use("lewis6991/gitsigns.nvim")
+	-- gitsigns
+	use("lewis6991/gitsigns.nvim")
 
-    -- markdownpreview
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = "cd app && npm install",
-        setup = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
-    })
+	-- markdownpreview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		setup = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	})
 
-    -- undo tree
-    use("mbbill/undotree")
+	-- undo tree
+	use("mbbill/undotree")
 
-    -- trouble pane
-    use("folke/trouble.nvim")
+	-- trouble pane
+	use("folke/trouble.nvim")
 
-    -- which key
-    use {
-      "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-      end
-    }
+	-- which key
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 
-    -- zen mode
-    use {
-      "folke/zen-mode.nvim",
-      config = function()
-        require("zen-mode").setup {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-      end
-    }
+	-- zen mode
+	use({
+		"folke/zen-mode.nvim",
+		config = function()
+			require("zen-mode").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 
-    if packer_boostrap then
-        require("packer").sync()
-    end
+	if packer_boostrap then
+		require("packer").sync()
+	end
 end)
