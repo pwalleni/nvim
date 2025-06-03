97676022560c73177424b2a1f74ba552bb76de23
Pascal Wallenius <44832734+pwalleni@users.noreply.github.com>
2025-06-03 20:21:31 +0200

    Fix typos in plugin configs

--- lua/pwalleni/plugins/lsp/lspsaga.lua
+++ lua/pwalleni/plugins/lsp/lspsaga.lua
@@ -3,8 +3,8 @@ local saga = require("lspsaga")
 saga.setup({
 	use_saga_diagnostic_sign = true,
 	error_sign = "",
-	warn_sign = "",
-	hintsign = " ",
-	infor_sign = "",
+    warn_sign = "",
+    hint_sign = " ",
+    infor_sign = "",
 	border_style = "round",
 })
